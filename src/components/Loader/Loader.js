be9d5f52853e6e6455ad64eef6a38f63bbbad6f5
fahadmak <fahad.makabugo@andela.com>
2019-03-03 16:26:05 +0300

    Bug(Login): Change Navlink
    - Change navigation in login component
    
    [Maintains ##164354929]

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export const Loader = () => <div class="lds-dual-ring"></div>
+
+export default Loader;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 import App from '../components/App';
 import Login from '../components/Login/Login';
 import SignUp from '../components/SignUp/SignUp';
+import { ToastContainer } from 'react-toastify';
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
+import '../css/toast.css';
 
 const Routes = () => <Router>
     <div>
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/" component={App} />
+        <Route exact path="/home" component={App} />
+        <ToastContainer />
     </div>
 </Router>;
 
