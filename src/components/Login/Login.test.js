eedcbfb96e41bcf584fa35b8c151376d5154a724
fahadmak <fahad.makabugo@andela.com>
2019-03-03 16:26:05 +0300

    Bug(Login): Change Navlink
    - Change navigation in login component
    
    [Maintains ##164354929]

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,85 +1,85 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
-import {Login} from './Login';
-import {mapStateToProps} from "./Login";
+import { Login } from './Login';
+import { mapStateToProps } from "./Login";
 
 describe(`Login`, () => {
 
-    let wrapper;
-    const mockLogin = jest.fn(),
-     historyMock = {push: jest.fn()};
+  let wrapper;
+  const mockLogin = jest.fn(),
+    historyMock = { push: jest.fn() };
 
-    beforeEach(() => {
+  beforeEach(() => {
 
-        wrapper = shallow(<Login fetchLogin={mockLogin} history={historyMock} />);
+    wrapper = shallow(<Login fetchLogin={mockLogin} history={historyMock} />);
 
-});
-    afterEach(() => {
+  });
+  afterEach(() => {
 
-        mockLogin.mock.calls = [];
+    mockLogin.mock.calls = [];
 
-});
-    describe('handlesubmit', () => {
+  });
+  describe('handlesubmit', () => {
 
-        it('should be call  fetchLogin with the name, username and password in the state as arguments', () => {
+    it('should be call  fetchLogin with the name, username and password in the state as arguments', () => {
 
-            wrapper.find('#username').simulate('change', {
-                target: {"username": "value"}
-            });
-            wrapper.find('form').simulate('submit', {preventDefault () { }});
-            expect(mockLogin.mock.calls.length).toEqual(1);
+      wrapper.find('#username').simulate('change', {
+        target: { "username": "value" }
+      });
+      wrapper.find('form').simulate('submit', { preventDefault() { } });
+      expect(mockLogin.mock.calls.length).toEqual(1);
 
-});
-        it('should will recieve signup props', () => {
+    });
+    it('should will recieve signup props', () => {
 
-            wrapper.setProps({message: 'your welcome'});
-            expect(wrapper.state('message')).toEqual('your welcome');
+      wrapper.setProps({ message: 'your welcome' });
+      expect(wrapper.state('message')).toEqual('your welcome');
 
-});
+    });
 
-});
+  });
 
 });
 
 describe('tests for <SignUpView> container', () => {
 
-    it('call signup once', () => {
+  it('call signup once', () => {
 
-        const spy = sinon.spy(Login.prototype, 'componentWillReceiveProps'),
-         wrapper = shallow(<Login
-                error={{
-                    error: {
-                        username: '',
-                        password: ''
-                    }
-                }}
-            />,);
+    const spy = sinon.spy(Login.prototype, 'componentWillReceiveProps'),
+      wrapper = shallow(<Login
+        error={{
+          error: {
+            username: '',
+            password: ''
+          }
+        }}
+      />);
 
-        expect(spy).toHaveProperty('callCount', 0);
-        wrapper.setProps({
-            error: {
-                error: {
-                    username: 'wrong id',
-                    password: ''
-                }
-            }
-        });
-        expect(spy).toHaveProperty('callCount', 1);
+    expect(spy).toHaveProperty('callCount', 0);
+    wrapper.setProps({
+      error: {
+        error: {
+          username: 'wrong id',
+          password: ''
+        }
+      }
+    });
+    expect(spy).toHaveProperty('callCount', 1);
 
-});
-    it('mapStateToProps should return the right value', () => {
+  });
+  it('mapStateToProps should return the right value', () => {
 
-        const mockedState = {
-            auth: {
-                error: {},
-                message: 'loggedIn'
-            }
-        },
-         state = mapStateToProps(mockedState);
+    const mockedState = {
+      auth: {
+        error: {},
+        message: 'loggedIn'
+      }
+    },
+      state = mapStateToProps(mockedState);
 
-        expect(state.error).toEqual({});
+    expect(state.error).toEqual({});
 
-});
+  });
 
 });
--- src/components/SignUp/SignUp.test.js
+++ src/components/SignUp/SignUp.test.js
@@ -1,85 +1,85 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
-import {SignUp} from './SignUp';
-import {mapStateToProps} from "./SignUp";
+import { SignUp } from './SignUp';
+import { mapStateToProps } from "./SignUp";
 
 describe(`SignUp`, () => {
 
-    let wrapper;
-    const mockSignUp = jest.fn(),
-     historyMock = {push: jest.fn()};
+  let wrapper;
+  const mockSignUp = jest.fn(),
+    historyMock = { push: jest.fn() };
 
-    beforeEach(() => {
+  beforeEach(() => {
 
-        wrapper = shallow(<SignUp fetchSignup={mockSignUp} history={historyMock} />);
+    wrapper = shallow(<SignUp fetchSignup={mockSignUp} history={historyMock} />);
 
-});
-    afterEach(() => {
+  });
+  afterEach(() => {
 
-        mockSignUp.mock.calls = [];
+    mockSignUp.mock.calls = [];
 
-});
-    describe('handlesubmit', () => {
+  });
+  describe('handlesubmit', () => {
 
-        it('should be call  fetchSignUp with the name, username and password in the state as arguments', () => {
+    it('should be call  fetchSignUp with the name, username and password in the state as arguments', () => {
 
-            wrapper.find('#username').simulate('change', {
-                target: {"username": "value"}
-            });
-            wrapper.find('form').simulate('submit', {preventDefault () { }});
-            expect(mockSignUp.mock.calls.length).toEqual(1);
+      wrapper.find('#username').simulate('change', {
+        target: { "username": "value" }
+      });
+      wrapper.find('form').simulate('submit', { preventDefault() { } });
+      expect(mockSignUp.mock.calls.length).toEqual(1);
 
-});
-        it('should will recieve signup props', () => {
+    });
+    it('should will recieve signup props', () => {
 
-            wrapper.setProps({message: 'your welcome'});
-            expect(wrapper.state('message')).toEqual('your welcome');
+      wrapper.setProps({ message: 'your welcome' });
+      expect(wrapper.state('message')).toEqual('your welcome');
 
-});
+    });
 
-});
+  });
 
 });
 
 describe('tests for <SignUpView> container', () => {
 
-    it('call signup once', () => {
+  it('call signup once', () => {
 
-        const spy = sinon.spy(SignUp.prototype, 'componentWillReceiveProps'),
-         wrapper = shallow(<SignUp
-                error={{
-                    error: {
-                        username: '',
-                        password: ''
-                    }
-                }}
-            />,);
+    const spy = sinon.spy(SignUp.prototype, 'componentWillReceiveProps'),
+      wrapper = shallow(<SignUp
+        error={{
+          error: {
+            username: '',
+            password: ''
+          }
+        }}
+      />);
 
-        expect(spy).toHaveProperty('callCount', 0);
-        wrapper.setProps({
-            error: {
-                error: {
-                    username: 'wrong id',
-                    password: ''
-                }
-            }
-        });
-        expect(spy).toHaveProperty('callCount', 1);
+    expect(spy).toHaveProperty('callCount', 0);
+    wrapper.setProps({
+      error: {
+        error: {
+          username: 'wrong id',
+          password: ''
+        }
+      }
+    });
+    expect(spy).toHaveProperty('callCount', 1);
 
-});
-    it('mapStateToProps should return the right value', () => {
+  });
+  it('mapStateToProps should return the right value', () => {
 
-        const mockedState = {
-            auth: {
-                error: {},
-                message: 'signed up'
-            }
-        },
-         state = mapStateToProps(mockedState);
+    const mockedState = {
+      auth: {
+        error: {},
+        message: 'signed up'
+      }
+    },
+      state = mapStateToProps(mockedState);
 
-        expect(state.error).toEqual({});
+    expect(state.error).toEqual({});
 
-});
+  });
 
 });
